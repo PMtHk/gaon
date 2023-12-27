74ea4512aa728d03e81ae24d2b6948f61ba19bfa
PMtHk <zooby88@naver.com>
2023-12-28 02:39:02 +0900

    fix : update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,23 +1,35 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
   content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   prefix: "",
   theme: {
     container: {
       center: true,
-      padding: "2rem",
       screens: {
-        "2xl": "1400px",
+        sm: "100%",
+        md: "100%",
+        lg: "1024px",
+        xl: "1280px",
       },
     },
     extend: {
+      backgroundImage: {
+        "home-intro": "url('/4772f52a-b7b0-4191-a6a9-bbcda3b1579a.png')",
+        "call-info": "url('/cda7811e-098c-4791-af64-770c353f7736.png')",
+        vc: "url('/f9496384-f741-408e-a9f7-0819fce106bc.png')",
+        vb: "url('/6120120b-54bb-4ac5-8ccb-6d1d8ef64acb.png')",
+        ranking: "url('/c161a433-8ded-4789-be91-e59121399f99.png')",
+        "care-tool": "url('/823ece4d-6728-4cfb-9561-1fe98db42022.png')",
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
       keyframes: {
         "accordion-down": {
           from: { height: "0" },
@@ -35,6 +47,6 @@ const config = {
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
