394ac9acbab52b765e07e5f3ed6c9c2ad5b722b0
PMtHk <zooby88@naver.com>
2024-02-18 18:27:41 +0900

    hotfix : update robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: '/',
       disallow: '/admin/',
     },
-    sitemap: 'https://gaoncare.com/sitemap.xml',
+    sitemap: 'https://gaoncare.kr/sitemap.xml',
   }
 }
