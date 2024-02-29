9e1f9dcc0501b91cc47541077f8f56dfa9a96a59
PMtHk <zooby88@naver.com>
2024-02-29 13:39:07 +0900

    chore : robots.txt - 네이버봇(Yeti) 추가

--- app/robots.ts
+++ app/robots.ts
@@ -1,12 +1,19 @@
-import { MetadataRoute } from 'next'
- 
+import { MetadataRoute } from "next";
+
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: '*',
-      allow: '/',
-      disallow: '/admin/',
-    },
-    sitemap: 'https://gaoncare.kr/sitemap.xml',
-  }
+    rules: [
+      {
+        userAgent: ["Applebot", "Bingbot", "Googlebot"],
+        allow: ["/"],
+        disallow: "/admin/",
+      },
+      {
+        userAgent: "Yeti",
+        allow: ["/"],
+        disallow: "/admin/",
+      },
+    ],
+    sitemap: "https://gaoncare.kr/sitemap.xml",
+  };
 }
